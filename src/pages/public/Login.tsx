5ef00f87ad6b72c06bf4e29ef7b5f21287fa442f
pablossrudi <pablossrudi@gmail.com>
2025-05-12 02:26:15 -0400

    corregir título y ajustar estilos en el formulario de inicio de sesión

--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -27,16 +27,14 @@ export const Login = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-transparent">
             <h1 className="text-3xl font-medium text-gray-700 mb-9">
-                Sistema de Calificaciones
+                Sistema de calificaciones
             </h1>
-            <div className="max-w-md w-full space-y-8 p-8 bg-primary shadow-primary hover:shadow-lg transition-shadow duration-300 ease-in-out">
-
+            <div className="max-w-sm w-full space-y-8 py-8 px-10 bg-primary shadow-primary hover:shadow-lg transition-shadow duration-300 ease-in-out">
                 <h1 className="text-2xl font-medium text-gray-700">
                     Bienvenid@!
                 </h1>
-
-                <form className="mt-14 space-y-6" onSubmit={handleSubmit}>
-                    <div className="flex flex-col space-y-6 mb-14">
+                <form className="mt-12 space-y-6" onSubmit={handleSubmit}>
+                    <div className="flex flex-col space-y-6 mb-16">
                         <div>
                             <h1 className="text-color-primary font-medium text-gray-700 mb-0.5">Nombre de usuario</h1>
                             <label htmlFor="username" className="sr-only"></label>
@@ -45,13 +43,12 @@ export const Login = () => {
                                 name="username"
                                 type="text"
                                 required
-                                className="relative block w-full px-3 py-2 bg-white placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                className="relative block w-full px-3 py-2 bg-white placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                 placeholder="Usuario"
                                 value={userName}
                                 onChange={(e) => setUserName(e.target.value)}
                             />
                         </div>
-
                         <div>
                             <h1 className="text-color-primary font-medium text-gray-700 mb-0.5">Contraseña</h1>
                             <label htmlFor="password" className="sr-only"></label>
@@ -60,7 +57,7 @@ export const Login = () => {
                                 name="password"
                                 type="password"
                                 required
-                                className="relative block w-full px-3 py-2 bg-white placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                className="relative block w-full px-3 py-2 bg-white placeholder-gray-400 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                 placeholder="••••••••••••"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
@@ -74,7 +71,7 @@ export const Login = () => {
                         </div>
                     )}
 
-                    <div className='flex justify-center'>
+                    <div className='flex justify-center mb-2'>
                         <button
                             type="submit"
                             className="p-3 w-48 bg-blue-100 text-blue-700 hover:bg-blue-500 hover:text-white text-base font-medium shadow-md hover:shadow-lg transition-shadow duration-300 cursor-pointer"
