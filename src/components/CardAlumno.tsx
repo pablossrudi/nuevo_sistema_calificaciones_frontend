d0e88ec380467f8821db77c00a390c2c3348f9d5
pablossrudi <pablossrudi@gmail.com>
2025-05-12 21:20:08 -0400

    agregar funcionalidad de eliminación de alumno con modal de confirmación en CardAlumno

--- src/components/CardAlumno.tsx
+++ src/components/CardAlumno.tsx
@@ -1,26 +1,50 @@
-
+import { useState } from 'react';
+import { alumnosService } from '../service/alumnos.service';
 import type { Alumno } from '../service/alumnos.service';
+import { ModalCentrado } from './modals/ModalCentrado';
 
-export const CardAlumno = ({ alumnoId, alumnoNombre, alumnoRut, alumnoDireccion, materias }: Alumno) => {
+interface Props {
+    alumno: Alumno
+    fechAlumnos: () => void
+}
+
+export const CardAlumno = ({ alumno, fechAlumnos }: Props) => {
+    const [error, setError] = useState('');
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleDeleteAlumno = async () => {
+        setError('');
+        try {
+            await alumnosService.deleteAlumno(alumno.alumnoId);
+            setModalOpen(false);
+            fechAlumnos();
+        } catch (err: any) {
+            setError(
+                err?.response?.data?.message ||
+                err?.message ||
+                'Error al conectar con el servidor. Por favor, intente nuevamente.'
+            );
+        }
+    };
     return (
         <div className="bg-primary shadow-primary overflow-hidden hover:shadow-lg transition-shadow duration-300">
             <div className="p-6 flex flex-col justify-between h-full mx-3">
                 <div>
                     <div className="mb-4">
-                        <h2 className="text-2xl font-medium text-gray-700">{alumnoNombre}</h2>
+                        <h2 className="text-2xl font-medium text-gray-700">{alumno.alumnoNombre}</h2>
                     </div>
                     <div className="text-gray-700">
                         <p className="text-base font-medium">
-                            Rut: {alumnoRut}
+                            Rut: {alumno.alumnoRut}
                         </p>
                         <p className="text-base font-medium">
-                            Dirección: {alumnoDireccion}
+                            Dirección: {alumno.alumnoDireccion}
                         </p>
                     </div>
                 </div>
                 <div className="mt-4">
                     <ul className="list-inside list-none">
-                        {materias.map((materia, index) => (
+                        {alumno.materias.map((materia, index) => (
                             <div className='flex justify-between shadow-primary p-3 mb-2' key={index}>
                                 <li key={index} className="text-base font-medium text-gray-700 ">{materia.materiaNombre}</li>
                                 <p className="text-base font-medium text-gray-700 cursor-pointer">Ver más</p>
@@ -31,18 +55,23 @@ export const CardAlumno = ({ alumnoId, alumnoNombre, alumnoRut, alumnoDireccion,
                 <div className="mt-4 flex justify-between space-x-4">
                     <button
                         className="p-3 w-full bg-red-100 text-red-700 hover:bg-red-500 hover:text-white text-base font-medium shadow-md hover:shadow-lg transition-shadow duration-300"
-                        onClick={() => console.log('Eliminar', alumnoId)}
+                        onClick={() => setModalOpen(true)}
                     >
                         Eliminar Alumno
                     </button>
                     <button
                         className="p-3 w-full bg-blue-100 text-blue-700 hover:bg-blue-500 hover:text-white text-base font-medium shadow-md hover:shadow-lg transition-shadow duration-300"
-                        onClick={() => console.log('Editar', alumnoId)}
+                        onClick={() => console.log('Editar', alumno.alumnoId)}
                     >
                         Editar Alumno
                     </button>
                 </div>
+                {/* <p>{error}</p> */}
             </div>
+            <ModalCentrado open={modalOpen} title='Eliminar Alumno' onClose={() => setModalOpen(false)} onConfirm={handleDeleteAlumno}>
+                <h2 className='text-xl font-bold text-gray-700'>Estas a punto de eliminar a {alumno.alumnoNombre}</h2>
+                <h2 className='text-xl font-bold text-gray-700'>¿Estas segur@ de ejecutar esta acción?</h2>
+            </ModalCentrado>
         </div>
     );
 };
